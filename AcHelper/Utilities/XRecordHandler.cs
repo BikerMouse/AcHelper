b04d52c07cee69592d2c131f684330aa452a4e1c
Sander van Bueren <s.vanbueren@gmail.com>
2016-06-16 15:59:21 +0200

    Created XRecordHandler.cs

---
+++ AcHelper/Utilities/XRecordHandler.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcHelper.Utilities
+{
+    public class XRecordHandler
+    {
+        #region Fields ...
+        private Database _database = null;
+        #endregion
+
+        #region Ctor ...
+        public XRecordHandler()
+            : this(Active.Document)
+        { }
+        public XRecordHandler(Document doc)
+        {
+            if (doc != null && doc.IsActive)
+            {
+                _database = doc.Database;
+            }
+        }
+        #endregion
+    }
+}
