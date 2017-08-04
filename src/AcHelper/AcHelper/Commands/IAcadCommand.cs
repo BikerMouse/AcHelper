6d69015580a7cc7ae66d8d0be6b32d1a7078b6ba
Sander van Bueren <s.vanbueren@gmail.com>
2017-08-04 14:53:58 +0200

    Cleaned up IAcadCommand and created SendStringToExecuteException

--- src/AcHelper/AcHelper/Commands/IAcadCommand.cs
+++ src/AcHelper/AcHelper/Commands/IAcadCommand.cs
@@ -1,11 +1,11 @@
 ﻿namespace AcHelper.Commands
 {
-    /// <summary>
-    /// The IAcadCommand interface is used to execute specific AutoCAD commands.
-    /// These commands will be executed through the <see cref="CommandHandlerBase.ExecuteCommand{T}"/>ExecuteCommand{T} method.
-    /// This way any uncaught error will be caught after all.
-    /// </summary>
-    public interface IAcadCommand
+	/// <summary>
+	/// The IAcadCommand interface is used to execute specific AutoCAD commands.
+	/// These commands will be executed through the <see cref="CommandHandlerBase.ExecuteCommand{T}"/>ExecuteCommand{T} method.
+	/// This way any uncaught error will be caught after all.
+	/// </summary>
+	public interface IAcadCommand
     {
         /// <summary>
         /// Executes the Command code.
